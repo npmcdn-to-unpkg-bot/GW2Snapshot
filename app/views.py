bc1c926e8588eaf7ca206e7b2913a40a32be73de
Murdius <garylau.work@gmail.com>
2016-06-28 19:05:42 -0400

    Fix

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from flask import Blueprint, render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('templates/timeline.html')
+    return render_template('templates/index.html')
